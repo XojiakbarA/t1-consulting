ad39a7d28cc1afb4bea41e1505d90242cef2eca2
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2023-10-14 14:37:55 +0500

    init

---
+++ src/main/java/com/example/t1consultingtask/T1ConsultingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.t1consultingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ConsultingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1ConsultingTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/t1consultingtask/T1ConsultingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t1consultingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ConsultingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
